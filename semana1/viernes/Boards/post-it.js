befb3e51a4e4160a7136beaaf4c4b06c4fa2a120
codeamac06 <bluespheal@gmail.com>
2017-03-16 14:05:05 -0600

    Avance de Board

--- semana1/viernes/Boards/post-it.js
+++ semana1/viernes/Boards/post-it.js
@@ -1,35 +1,70 @@
 var Boarder = function( ){
 
+  var title = $( "#board_title" );
+  board_counter = 0;
+
+  var dialog = $( "#dialog" ).dialog({   
+    autoOpen: false,
+    modal: true,
+    buttons: {
+      OK: function(){
+        addBoard();
+        $(this).dialog("close");
+      },
+      Cancel: function(){
+        $(this).dialog("close");        
+      }
+    },
+    close: function(){
+      form[0].reset();
+    }
+  });
+
+  var form = dialog.find( "form" ).on( "submit", function( event ) {
+    addBoard();
+    dialog.dialog( "close" );
+    event.preventDefault();
+  });
+
+  var dialogo =  $("#new_board").on("click", function(){
+      $("#dialog").dialog("open");
+  });
+
   function initialize() {
     $("#master").css("visibility", "hidden");
 
   };
 
-  initialize();
+  function addBoard() {
+    $("#boards_list").append("<li class = 'boarder_li' id= '"+ board_counter +"'> "+ title.val() +" </li>")
+    $("#whiteboard").append(new Board("#board-"+ board_counter));
+    board_counter++;
+    $(".boarder_li").on("click",function(event){
+      event.preventDefault();
+      var iden= $(this).attr("id");
+      $(".board").css("visibility","hidden");
+      $("#board-"+iden).css("visibility","visible");
+    });
+  }
 
-
-  $("#new_board").click(function(event) {
-     $( "#add_tab" ).on( "click", function() {
-        dialog.dialog( "open" );
-      });
-    // $("#master_board").append(new Board("#board"));
-  });
-
+  initialize(); 
 };
 
 var Board = function( selector ) {
 
-  $("#master_board").append('<div id="board"><div id="master" class="post-it"><div class="header"><div class="close">X</div></div><div class="content">...</div></div><div/>');
+  $("#whiteboard").append('<div class= "board" id="board-'+board_counter+'"><div/>');
 
 
-  $("#board").dblclick(function() {
+  $(selector).dblclick(function(event) {
+    alert(selector+ "///"+ this);
+    event.stopPropagation();
     $(this).append(new PostIt);
   });
 };
 
 var PostIt = function() {
 
-  $("#board").append('<div id="master" class="post-it"><div class="header"><div class="close">X</div></div><div class="content">...</div></div>');
+  $(".board").append('<div id="master" class="post-it"><div class="header"><div class="close">X</div></div><div class="content">...</div></div>');
   $(".post-it:last-child").css ({"left": event.pageX, "top": event.pageY});
   $(".content").attr('contenteditable', "true");
   $(".header").attr('contenteditable', "true");
@@ -57,3 +92,5 @@ $(function() {
   // Esta es la fucnión que correrá cuando este listo el DOM 
   new Boarder();
 });
+
+
